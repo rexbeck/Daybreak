59dd1f92c72ac083a3c1813386c6435d73729b00
rexbeck <reaganrbeck@gmail.com>
2020-01-07 11:27:39 -0500

    First Commit

--- Daybreak/ViewController.swift
+++ Daybreak/ViewController.swift
@@ -2,18 +2,46 @@
 //  ViewController.swift
 //  Daybreak
 //
-//  Created by  on 1/2/20.
+//  Created by Rex Beck on 1/2/20.
 //  Copyright © 2020 bruhapps. All rights reserved.
 //
 
 import UIKit
+import CalendarKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
+    
+//    // Return an array of EventDescriptors for particular date
+//    func eventsForDate(_ date: Date) -> [EventDescriptor]
+//    {
+//             var models = // Get events (models) from the storage / API
+//
+//             var events = [Event]()
+//
+//             for model in models {
+//                 // Create new EventView
+//                 let event = Event()
+//                 // Specify StartDate and EndDate
+//                 event.startDate = model.startDate
+//                 event.endDate = model.endDate
+//                 // Add info: event title, subtitle, location to the array of Strings
+//                 var info = [model.title, model.location]
+//                 info.append("\(datePeriod.beginning!.format(with: "HH:mm")) - \(datePeriod.end!.format(with: "HH:mm"))")
+//                 // Set "text" value of event by formatting all the information needed for display
+//                 event.text = info.reduce("", {$0 + $1 + "\n"})
+//                 events.append(event)
+//             }
+//             return events
+//    }
+    
 
 
 }
